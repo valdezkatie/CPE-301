69147b74a61843cb6371f0be5573356105ff7635
valdezkatie <kaitlynvaldez@unr.edu>
2025-04-23 14:44:10 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -27,3 +27,5 @@ volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
 volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+
+//hi
